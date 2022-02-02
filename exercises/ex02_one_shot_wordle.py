f493cecd20016439ccdbd2689b2e3c93c3d591e0
Miles Murphy <milesfmu@ad.unc.edu>
2022-02-02 18:22:43 -0500

    ex02 saved

---
+++ exercises/ex02_one_shot_wordle.py
@@ -0,0 +1,43 @@
+"""One shot- Don't choke."""
+__author__ = "730522786"
+
+WHITE_BOX: str = "\U00002B1C"
+GREEN_BOX: str = "\U0001F7E9"
+YELLOW_BOX: str = "\U0001F7E8"
+box: str = ""
+secret_word: str = "python"
+guess: str = input(f"What is your {len(secret_word)}-letter guess? ")
+
+while len(guess) != len(secret_word):
+    guess: str = input(f"That was not {len(secret_word)} letters! Try again: ")
+
+
+guess_chr: int = 0
+guessed_chr_present = False
+alt_indices: int = 0
+while guess_chr < len(secret_word):
+    if guess[guess_chr] == secret_word[guess_chr]:
+        box = box + GREEN_BOX
+    else:
+        while alt_indices < len(secret_word) and guessed_chr_present is False:
+            if guess[guess_chr] == secret_word[alt_indices]:
+                guessed_chr_present = True
+            alt_indices = alt_indices + 1
+        if guessed_chr_present is True:
+            box = box + YELLOW_BOX
+        else:
+            box = box + WHITE_BOX
+    guessed_chr_present = False
+    alt_indices = 0
+    guess_chr = guess_chr + 1
+
+print(box)
+
+if len(guess) == len(secret_word):
+
+    if guess == secret_word:
+        print("Woo! You got it!")
+        quit()
+    if guess != secret_word:
+        print("Not quite. Play again soon!")
+    
